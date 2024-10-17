469f5661f9f1c05d1bc4cf6144a077fbf9294882
Ying L <forgmina@gmail.com>
2024-10-17 21:25:37 +0100

    added product app

---
+++ hsinwang/product/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Cake, CakeSize, FlavorOption
+
+admin.site.register(Cake)
+admin.site.register(CakeSize)
+admin.site.register(FlavorOption)
---
+++ hsinwang/product/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+class Cake(models.Model):
+    name = models.CharField(max_length=100)  
+    description = models.TextField(blank=True) 
+    image = models.ImageField(upload_to='cakes/', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class CakeSize(models.Model):
+    cake = models.ForeignKey(Cake, on_delete=models.CASCADE, related_name='sizes')  
+
+    base_price = models.DecimalField(max_digits=10, decimal_places=2) 
+
+    def __str__(self):
+        return f"{self.cake.name} - {self.size} - ${self.base_price}"
+
+class FlavorOption(models.Model):
+    cake = models.ForeignKey(Cake, on_delete=models.CASCADE, related_name='flavor_options') 
+    name = models.CharField(max_length=100) 
+    additional_price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.cake.name} - {self.name} +${self.additional_price}"
+
