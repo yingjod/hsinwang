100c16e51f53eb298771e3554b15cdb608488c2c
Ying L <forgmina@gmail.com>
2025-01-24 18:30:59 +0000

    fixed to the local

--- hsinwang/hsinwang/settings.py
+++ hsinwang/hsinwang/settings.py
@@ -24,6 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-s=#fwxwli%7akd^4iql)ris8gir*gy0b7t)+8bo1-iv#+78b@0"
 
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
 
 DEBUG = os.getenv('DJANGO_DEBUG', 'True') == 'True'
 
